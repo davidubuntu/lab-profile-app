fa49bd1a75903175231d23b0962b6fd9d21f6e32
David Diez <ddiezr@gmail.com>
2018-12-10 10:24:19 +0100

    take all data profile data

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -16,15 +16,20 @@ export default class Profile extends Component {
   componentDidMount() {
     this.auth
       .loggedin()
-      .then(res =>{this.setState({...this.state,    user:res});console.log(res)})
+      .then(res =>{this.setState({...this.state,  user:res});console.log(res)})
       .catch(err => console.log(err));
   }
 
   render() {
     return this.state.user ? (
       <div>
-        <h1> Profile </h1>
-        {this.state.user.username}
+        <h1> Profile Details </h1>
+        <ul>
+            <li><span>Username: </span>{this.state.user.username}</li>
+            <li><span>email: </span>{this.state.user.course}</li>
+        </ul>
+      
+       
       </div>
     ) : (
       <h1>No logedin</h1>
